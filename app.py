f93346f520ac88b26f77297c367e130d69ef5a25
stella03aportnoi-png <stella.03aportnoi@gmail.com>
2026-01-23 14:17:59 -0300

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import google.generativeai as genai
 
 # --- CONFIGURAÇÃO DA PÁGINA ---
-st.set_page_config(page_title="IA Sevenspeed", page_icon="🏎️", layout="wide")
+st.set_page_config(page_title="IA da Seven", page_icon="🏎️", layout="wide")
 
 st.title("🏎️ Chatbot Oficial Sevenspeed")
 st.markdown("Sou a IA da escuderia seven, estou aqui para te ajudar!")
@@ -78,6 +78,7 @@ if prompt := st.chat_input("Qual a dúvida sobre o carro ou regras?"):
         st.session_state.messages.append({"role": "assistant", "content": response.text})
     except Exception as e:
         st.error(f"Erro na resposta: {e}")
+
 
 
 
