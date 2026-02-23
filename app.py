b3304c6d3f21c6311656498e09362e48d99d8f2b
stella03aportnoi-png <stella.03aportnoi@gmail.com>
2026-02-22 22:34:27 -0300

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 # --- CONFIGURAÇÃO DA PÁGINA ---
 st.set_page_config(page_title="IA da Seven", page_icon="🏎️", layout="wide")
 
-st.title("🏎️ Chatbot Oficial Escuderia Seven")
+st.title("🏎️ IA Oficial Escuderia Seven")
 st.markdown("Sou a IA da escuderia Seven, estou aqui para te ajudar!")
 
 # --- CONFIGURAÇÃO DA API ---
@@ -79,6 +79,7 @@ if prompt := st.chat_input("Qual a dúvida sobre o carro ou regras?"):
         st.session_state.messages.append({"role": "assistant", "content": response.text})
     except Exception as e:
         st.error(f"Erro na resposta: {e}")
+
 
 
 
